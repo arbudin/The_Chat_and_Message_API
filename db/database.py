b2cb16b04ff23409295c087dcbfd93881dbb9825
Артем Будин <botak97x@gmail.com>
2026-01-22 22:55:44 +0500

    Обновил .gitignore

--- db/database.py
+++ db/database.py
@@ -1,11 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-db_user = 'postgres'
-db_password = ''
-db_host = 'localhost'
-db_port = '5432'
-db_name = 'chats_and_messages_db'
+load_dotenv()
+
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
 
 # ссылка на БД
 DATABASE_URL = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
